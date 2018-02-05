965a6e6d94aa53551d465265260c55ccffd8c193
Augustin Voisin-Marras <augustinvoima@gmail.com>
2018-02-05 13:50:53 +0100

    correcting script newlines escape

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,9 @@ cd ~/syncserver;
 
 read -p "Enter public url: " PUB_URL;
 read -p "SQLAlchemy database URI (use @localhost for a database stored on this container's host): " DATABASE_URI;
-$(ip route show | grep default | cut -f 3 -d " ")
-crudini --set --format=ini syncserver.ini syncserver
+crudini --set --format=ini syncserver.ini syncserver \
   public_url $PUB_URL;
-crudini --set --format=ini syncserver.ini syncserver
-  sqluri $(echo $DATABASE_URI |
-    sed -e "s/@localhost/
-    $(ip route show | grep default | cut -f 3 -d " ")/g");
+crudini --set --format=ini syncserver.ini syncserver \
+  sqluri $(echo $DATABASE_URI | \
+  sed -e "s/@localhost/$(ip route show | grep default | cut -f 3 -d " ")/g");
 cd ~/syncserver && make serve;
