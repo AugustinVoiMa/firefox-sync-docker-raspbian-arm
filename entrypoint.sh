68b400523dd9d8af62ef09de2bc80f011bd6b7fc
Augustin Voisin-Marras <augustinvoima@gmail.com>
2018-02-05 15:10:09 +0100

    Ok

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 cd ~/syncserver;
 
 read -p "Enter public url: " PUB_URL;
-read -p "SQLAlchemy database URI (use @localhost for a database stored on this container's host): " DATABASE_URI;
+read -p "SQLAlchemy database URI: " DATABASE_URI;
 crudini --set --format=ini syncserver.ini syncserver \
   public_url $PUB_URL;
+
 crudini --set --format=ini syncserver.ini syncserver \
-  sqluri $(echo $DATABASE_URI | \
-  sed -e "s/@localhost/$(ip route show | grep default | cut -f 3 -d " ")/g");
+  sqluri $DATABASE_URI;
 cd ~/syncserver && make serve;
