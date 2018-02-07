b00bc9cc0c479a7b5e40a32097326b7a8e42f2d0
Augustin Voisin-Marras <augustinvoima@gmail.com>
2018-02-07 11:15:45 +0100

    using default gateway

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,14 @@
 cd ~/syncserver;
 
 read -p "Enter public url: " PUB_URL;
-read -p "SQLAlchemy database URI: " DATABASE_URI;
+read -p "SQLAlchemy database URI (container's gateway $(ip route show | grep default | cut -f 3 -d " ") will substitute %dockerhost):" DATABASE_URI;
 crudini --set --format=ini syncserver.ini syncserver \
   public_url $PUB_URL;
 
 crudini --set --format=ini syncserver.ini syncserver \
-  sqluri $DATABASE_URI;
+  sqluri $(echo $DATABASE_URI | \
+  sed -e "s/%dockerhost/$(ip route show | grep default | cut -f 3 -d " ")/g");
 
 crudini --set --format=ini syncserver.ini auth allow_new_users true;
-
+echo "Configuration ok";
 cd ~/syncserver && make serve;
